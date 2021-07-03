b6c240b7989e2d869f2527ec075ed3f1920d5437
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-07-03 17:18:29 +0530

    start skeleton

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const {Client} = require('pg')
+const ConsistentHash = require('consistent-hash')
+const crypto = require('crypto')
+const ring  = new ConsistentHash()
+const app = express()
+
+const clients = {
+    5432: new Client({
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: 'password',
+        database: 'postgres'
+    }),
+    5433: new Client({
+        host: 'localhost',
+        port: 5433,
+        user: 'postgres',
+        password: 'password',
+        database: 'postgres'
+    }),
+    5434: new Client({
+        host: 'localhost',
+        port: 5434,
+        user: 'postgres',
+        password: 'password',
+        database: 'postgres'
+    })
+}
+
+Object.entries(clients).forEach(client => {
+    ring.add(client[0])
+})
+
+async function connect() {
+    Object.entries(clients).forEach(async (client) => {
+        await client[1].connect()
+    })
+}
+
+app.get('/:urlId', async (req, res) => {
+    const {urlId} = req.params
+    const dbConnection = ring.get(urlId)
+    const response = await clients[dbConnection].query('select * from url_table where url_id = $1', [urlId])
+    if(response.rowCount < 1) {
+        return res.statusCode(404)
+    }
+    res.send(response.rows[0])
+})
+
+app.post('/', async (req, res) => {
+    const {url} = req.query
+    const hash = crypto.createHash('sha512').update(url).digest('base64')
+    const urlId = hash.substr(0, 15)
+    const dbConnection = ring.get(urlId)
+    await clients[dbConnection].query('insert into url_table (url, url_id) values ($1, $2)', [url, urlId])
+    res.send({
+        'hash': hash,
+        'urlId': urlId,
+        'server': dbConnection
+    })
+})
+
+app.listen(8080, () => {
+    console.log('connected to 8080')
+})
